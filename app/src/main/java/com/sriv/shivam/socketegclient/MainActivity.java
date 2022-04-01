06ce6bdd6b7be0c0ab0fcf6da80af10cf6888643
Shivam Srivastava <shivam.362000.1@gmail.com>
2022-04-01 18:17:55 +0530

    Input for coordinates added

--- app/src/main/java/com/sriv/shivam/socketegclient/MainActivity.java
+++ app/src/main/java/com/sriv/shivam/socketegclient/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
     // declaring required variables
     private Socket client;
     private PrintWriter printwriter;
-    private String message;
-    EditText messageEditText;
+    private String message, xString, yString;
+    EditText messageEditText, xAxis, yAxis;
     Button sendButton;
     final String SERVER_IP = "10.2.249.143";
 
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        xAxis = findViewById(R.id.xAxis);
+        yAxis = findViewById(R.id.yAxis);
         messageEditText = findViewById(R.id.messageEditText);
         sendButton = findViewById(R.id.sendButton);
 
@@ -34,10 +36,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 message = messageEditText.getText().toString();
+                xString = xAxis.getText().toString();
+                yString = yAxis.getText().toString();
                 Log.i("test", "On click");
 
                 // start the Thread to connect to server
-                new Thread(new ClientThread(message)).start();
+                new Thread(new ClientThread(message, xString, yString)).start();
             }
         });
     }
@@ -45,12 +49,22 @@ public class MainActivity extends AppCompatActivity {
     // the ClientThread class performs
     // the networking operations
     class ClientThread implements Runnable {
+        String finalMessage;
         private final String message;
+        private final String xString;
+        private final String yString;
 
-        ClientThread(String message) {
+        public ClientThread(String message, String xString, String yString) {
             this.message = message;
+            this.xString = xString;
+            this.yString = yString;
+
+            finalMessage = this.xString + "&" + this.yString;
+
             Log.i("test", "Message: " + this.message);
+            Log.i("test", finalMessage);
         }
+
         @Override
         public void run() {
             try {
@@ -59,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 // Creates a stream socket and connects it to the specified port number on the named host.
                 client = new Socket(SERVER_IP, 47430);  // connect to server
                 printwriter = new PrintWriter(client.getOutputStream(),true);
-                printwriter.write(message);  // write the message to output stream
+                printwriter.write(finalMessage);  // write the message to output stream
 
                 printwriter.flush();
                 printwriter.close();
